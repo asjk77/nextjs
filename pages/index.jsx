261a936a336482b2094d8854f6e9a8c9d73899af
MacBook <san@MacBookui-MacBook.local>
2021-05-26 14:05:38 +0900

    0.0.2 bug fix3

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import SiteGuard from '../components/siteGuard';
+import SiteGuard from '../components/SiteGuard';
 import Home from '../pages/home';
 
 
-var site_gard_on = false; 
-
 export default function Index(){
 
-  return site_gard_on ? <SiteGuard/> : <Home/>;
+  return <SiteGuard/>;
 }
