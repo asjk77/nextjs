942a2fabbb8f42f0f352668b276e2ba4d40db121
MacBook <san@MacBookui-MacBook.local>
2021-05-28 14:30:36 +0900

    0.0.3 bug fix1

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => (
     <ul className={Styles.container}>
         <li className={Styles.item}><Link href="/"><a>Home</a></Link><div></div></li>
         <li className={Styles.item}><Link href="/details"><a>Details</a></Link><div></div></li>
-        <li className={Styles.item}><Link href="/portfolio/main"><a>portfolio</a></Link><div></div></li>
+        <li className={Styles.item}><Link href="/portfolio/main"><a>Portfolio</a></Link><div></div></li>
         <li className={Styles.item}><Link href="/tech"><a>TechStack</a></Link><div></div></li>
     </ul>
 )
